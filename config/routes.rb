02c890ad4744fc321b7c7ab79f0b2989379d94af
Takuto <142597398+rapidcplus@users.noreply.github.com>
2024-04-24 15:39:58 +0900

    DBと各モデルを作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :users, only: %i[new create]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#top'
+
+  get :sign_up, to: 'users#new'
+  post :sign_up, to: 'users#create'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  get 'tags/:tag_name', to: 'items#index', as: :tag
+  
+  # resources :items do
+  #   collection do
+  #     get 'search' # postsのコレクションルートとしてsearchアクションを追加
+  #   end
+  #   resources :comments, module: :posts
+  # end
+
+  if Rails.env.development? || Rails.env.test?
+    get 'login_as/:user_id', to: 'development/sessions#login_as'
+  end
 end
